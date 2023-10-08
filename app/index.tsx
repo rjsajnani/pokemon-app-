034d5288d06ab057133fdce7fa8d24b749b66c6a
Raj Sajnani <rajsajnani43@gmail.com>
2023-10-08 14:53:18 +0200

    refactor: use PokemonList in home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,54 +1,45 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import {
-  View,
-  TouchableOpacity,
-  FlatList,
-  ActivityIndicator,
-  SafeAreaView,
-} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, ActivityIndicator, SafeAreaView } from 'react-native';
 
 import { Styles as styles } from './index.style';
-import { PokemonsList, getPokemons } from '../src/api';
-import { Card, Text } from '../src/components';
-import { useRouter } from 'expo-router';
+import { getPokemonDetailsByUrl, getPokemons } from '../src/api';
+import { Pokemon, PokemonList, Text } from '../src/components';
 import useCapitalizedString from '../src/hooks/useCapitalizedString';
 
-const gap = 10;
-
 export default function App() {
-  const [loading, setLoading] = useState(false);
-  const [results, setResults] = useState<any>([]);
-  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [nextUrl, setNextUrl] = useState<string | null>(null);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
   const capitalizedString = useCapitalizedString();
 
   useEffect(() => {
-    setLoading(true);
-    getPokemons().then((data) => {
-      setTimeout(() => {
-        setLoading(false);
-        setResults(data);
-      }, 2000);
-    });
+    (async () => {
+      setLoading(true);
+      await loadPokemons();
+    })();
   }, []);
 
-  const showDetail = () => {
-    console.log('show pokemon detail');
-  };
-
-  const renderItem = ({ item }: { item: PokemonsList }) => {
-    return (
-      <TouchableOpacity
-        onPress={() => {
-          router.push({
-            pathname: 'details/id/',
-            params: { id: '1', name: item.name },
-          });
-        }}
-      >
-        <Card name={capitalizedString(item.name)} />
-      </TouchableOpacity>
-    );
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemons(nextUrl);
+      setNextUrl(response.nextUrl);
+      const pokemonsArray = [];
+      for await (const pokemon of response.pokemonList) {
+        const pokemonDetails = await getPokemonDetailsByUrl(pokemon.url);
+        pokemonsArray.push({
+          url: pokemonDetails.species.url,
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          image: pokemonDetails.sprites.other['official-artwork'].front_default,
+        });
+      }
+      setPokemons([...pokemons, ...pokemonsArray]);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
   };
 
   return (
@@ -61,15 +52,11 @@ export default function App() {
         ) : (
           <>
             <Text text="Pokemon List" presets="header1" style={styles.title} />
-            {results.length > 0 && (
-              <FlatList
-                renderItem={renderItem}
-                data={results}
-                numColumns={2}
-                keyExtractor={(item) => item.name}
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ gap }}
-                columnWrapperStyle={{ gap }}
+            {pokemons.length > 0 && (
+              <PokemonList
+                pokemons={pokemons}
+                loadPokemons={loadPokemons}
+                isNext={!!nextUrl}
               />
             )}
           </>
