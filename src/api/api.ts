0616248534b54b70a4df9e87836e80a22525b0a5
Raj Sajnani <rajsajnani43@gmail.com>
2023-10-08 15:49:50 +0200

    refactor: add support to get individual pokemon details

--- src/api/api.ts
+++ src/api/api.ts
@@ -39,3 +39,15 @@ export async function getPokemonDetailsByUrl(url: string) {
     throw error;
   }
 }
+
+export async function getPokemonDetailsById(id: string | string[] | undefined) {
+  try {
+    const url = `${POKEMON_URL}/${id}`;
+    const response = await fetch(url);
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
