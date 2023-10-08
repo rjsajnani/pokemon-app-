e9055932639f9e970e038fa94cb43432a8717623
Raj Sajnani <rajsajnani43@gmail.com>
2023-10-08 17:43:11 +0200

    refactor: add searchInput in home screen

--- app/index.style.ts
+++ app/index.style.ts
@@ -3,12 +3,16 @@ import { StyleSheet } from 'react-native';
 export const Styles = StyleSheet.create({
   text: { fontSize: 14, textAlign: 'center' },
   container: {
+    paddingTop: 10,
     paddingHorizontal: 15,
     flex: 1,
     backgroundColor: '#fff',
   },
-  title: {
-    marginBottom: 10,
-  },
   loadingContainer: { justifyContent: 'center', alignItems: 'center', flex: 1 },
+  searchInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { View, ActivityIndicator, SafeAreaView } from 'react-native';
+import { View, ActivityIndicator, SafeAreaView, TextInput } from 'react-native';
 
 import { Styles as styles } from './index.style';
-import { getPokemonDetailsByUrl, getPokemons } from '../src/api';
-import { Pokemon, PokemonList, Text } from '../src/components';
-import useCapitalizedString from '../src/hooks/useCapitalizedString';
+import {
+  PokemonsList,
+  getAllPokemons,
+  getPokemonDetailsByUrl,
+  getPokemons,
+} from '../src/api';
+import { Pokemon, PokemonList, SearchInput, Text } from '../src/components';
+
+type PokemonDetails = {
+  url: string;
+  id: number;
+  name: string;
+  image: string;
+};
 
 export default function App() {
   const [loading, setLoading] = useState<boolean>(false);
   const [nextUrl, setNextUrl] = useState<string | null>(null);
+  const [searchText, setSearchText] = useState<string>('');
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
-  const capitalizedString = useCapitalizedString();
+  const [pokemonsToFind, setPokemonsToFind] = useState<Pokemon[]>([]);
 
   useEffect(() => {
     (async () => {
@@ -20,25 +31,60 @@ export default function App() {
     })();
   }, []);
 
+  const createPokemonDetails = async (
+    pokemons: PokemonsList[],
+  ): Promise<PokemonDetails[]> => {
+    const pokemonsArray: PokemonDetails[] = [];
+    for await (const pokemon of pokemons) {
+      const pokemonDetails = await getPokemonDetailsByUrl(pokemon.url);
+      pokemonsArray.push({
+        url: pokemonDetails.species.url,
+        id: pokemonDetails.id,
+        name: pokemonDetails.name,
+        image: pokemonDetails.sprites.other['official-artwork'].front_default,
+      });
+    }
+    return pokemonsArray;
+  };
+
   const loadPokemons = async () => {
     try {
       const response = await getPokemons(nextUrl);
       setNextUrl(response.nextUrl);
-      const pokemonsArray = [];
-      for await (const pokemon of response.pokemonList) {
-        const pokemonDetails = await getPokemonDetailsByUrl(pokemon.url);
-        pokemonsArray.push({
-          url: pokemonDetails.species.url,
-          id: pokemonDetails.id,
-          name: pokemonDetails.name,
-          image: pokemonDetails.sprites.other['official-artwork'].front_default,
-        });
-      }
+      const pokemonsArray: PokemonDetails[] = await createPokemonDetails(
+        response.pokemonList,
+      );
+
       setPokemons([...pokemons, ...pokemonsArray]);
       setLoading(false);
     } catch (error) {
       setLoading(false);
       console.error(error);
+    }
+  };
+
+  const findPokemon = async () => {
+    try {
+      let response = await getAllPokemons();
+      response = response.filter((pokemon: any) =>
+        pokemon.name.toLowerCase().includes(searchText.toLowerCase()),
+      );
+      const pokemonsArray: PokemonDetails[] = await createPokemonDetails(
+        response,
+      );
+
+      setPokemonsToFind(pokemonsArray);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  };
+
+  const onChangeSearch = (query: string) => {
+    setSearchText(query);
+    if (query === '') {
+      setPokemonsToFind([]);
     }
   };
 
@@ -51,12 +97,21 @@ export default function App() {
           </View>
         ) : (
           <>
-            <Text text="Pokemon List" presets="header1" style={styles.title} />
+            <Text text="Pokemon List" presets="header1" />
+            <SearchInput
+              onChangeSearch={onChangeSearch}
+              findPokemon={findPokemon}
+              searchText={searchText}
+            />
             {pokemons.length > 0 && (
               <PokemonList
-                pokemons={pokemons}
+                pokemons={
+                  searchText.length >= 1 && pokemonsToFind.length >= 1
+                    ? pokemonsToFind
+                    : pokemons
+                }
                 loadPokemons={loadPokemons}
-                isNext={!!nextUrl}
+                isNext={pokemonsToFind.length > 0 ? false : !!nextUrl}
               />
             )}
           </>
